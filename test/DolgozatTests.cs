dd81a778e8e02135b5343f5d9bfd5907764f12cd
Sz-Flora <szabof@kkszki.hu>
2026-05-19 11:53:10 +0200

    Add files via upload

---
+++ test/DolgozatTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DolgozatCLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozatCLI.Tests
+{
+    [TestClass()]
+    public class DolgozatTests
+    {
+        [TestMethod()]
+        public void otoserdemJegyTest()
+        {
+            Dolgozat otosdolgozat = new Dolgozat("Valaki", 18, 92);
+            Assert.AreEqual(5, otosdolgozat.erdemJegy());
+        }
+
+        [TestMethod()]
+        public void nemjoerdemJegyTest()
+        {
+            Dolgozat otosdolgozat = new Dolgozat("Valaki", 18, 92);
+            Assert.AreNotEqual(4, otosdolgozat.erdemJegy());
+        }
+
+        [TestMethod()]
+        public void egyeserdemJegyTest()
+        {
+            Dolgozat otosdolgozat = new Dolgozat("Valaki", 18, 10);
+            Assert.AreEqual(1, otosdolgozat.erdemJegy());
+        }
+    }
+}
